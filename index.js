dfa0fbf8f8bd40bca0cc503c6ba2a853c485755b
divyesh9572 <divyeshbarvadiya6@gmail.com>
2025-02-24 22:26:58 +0530

    a

--- index.js
+++ index.js
@@ -19,8 +19,15 @@ const axiosInstance = axios.create({
   headers: {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    Connection: "keep-alive",
-  },
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.nseindia.com/",
+    "Origin": "https://www.nseindia.com",
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
+    "Connection": "keep-alive",
+  }
+  
 });
 
 
